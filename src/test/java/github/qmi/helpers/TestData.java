51655e4900a9f1e877796defc5e2190a67921c5c
Inna <qmi4test@gmail.com>
2021-12-13 02:29:15 +0300

    init commit for files tests

---
+++ src/test/java/github/qmi/helpers/TestData.java
@@ -0,0 +1,31 @@
+package github.qmi.helpers;
+
+public class TestData {
+
+    public final static String UPLOAD_URL = "https://dropmefiles.com";
+    public final static String UPLOAD_SELECTOR = "input[type='file']";
+    public final static String UPLOAD_FILE_NAME = "textExample.txt";
+    public final static String UPLOADED_SELECTOR = ".percent";
+    public final static String UPLOAD_NAME = "загружено";
+
+    public final static String CSV_FILE_NAME = "csvExample.csv";
+
+    public final static String ZIP_FILE_NAME = "zipExample.zip";
+
+    public final static String TXT_URL = "https://tululu.org/b46195/";
+    public final static String TXT_SELECTOR = "a[href*='txt.php?id=46195'";
+    public final static String TXT_TEXT = "Экстремальное программирование";
+
+    public final static String PDF_URL = "https://nukadeti.ru/skazki/kradenoe_solnce";
+    public final static String PDF_SELECTOR = ".d-pdf";
+    public final static String PDF_TEXT = "Краденое солнце";
+
+    public final static String XLS_URL = "https://www.termoros.com/buyers/prices/?PAGEN_2=2";
+    public final static String XLS_SELECTOR = "a[href*='/upload/iblock/016/Hajdu.-Prays_list-obshchiy-s-01.09.2021.xls'";
+    public final static String XLS_TEXT = "Серия Z - твердотопливный котел";
+
+    public final static String ZIP_URL = "https://tululu.org/b46195/";
+    public final static String ZIP_SELECTOR = "a[href*='zip.php?id=46195'";
+    public final static String ZIP_TEXT = "46195.txt";
+
+}
---
+++ src/test/java/github/qmi/tests/FileTests.java
@@ -0,0 +1,128 @@
+package github.qmi.tests;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static github.qmi.helpers.TestData.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FileTests extends TestBase {
+
+    @Test
+    @DisplayName("Test for upload file")
+    @Owner("tsareva")
+    @Story("Upload file and check it on site")
+    void uploadFileResources() {
+        open(UPLOAD_URL);
+        $(UPLOAD_SELECTOR).uploadFromClasspath(UPLOAD_FILE_NAME);
+        $(UPLOADED_SELECTOR).shouldHave(text(UPLOAD_NAME));
+    }
+
+    @Test
+    @DisplayName("Test for CSV file")
+    @Owner("tsareva")
+    @Story("Parse and check CSV file")
+    void downloadFileCSV() throws IOException, CsvException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        try (InputStream is = classLoader.getResourceAsStream(CSV_FILE_NAME)) {
+            assert is != null;
+            try (Reader reader = new InputStreamReader(is)) {
+                CSVReader csvReader = new CSVReader(reader);
+
+                List<String[]> strings = csvReader.readAll();
+                assertEquals(7, strings.size());
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Test for ZIP file")
+    @Owner("tsareva")
+    @Story("Parse and check ZIP file")
+    void parseZipFileTest() throws IOException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        try (InputStream is = classLoader.getResourceAsStream(ZIP_FILE_NAME)) {
+            assert is != null;
+            try (ZipInputStream zis = new ZipInputStream(is)) {
+                ZipEntry entry;
+                while ((entry = zis.getNextEntry()) != null) {
+                    System.out.println(entry.getName());
+                }
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Test for txt file")
+    @Owner("tsareva")
+    @Story("Download, parse and check txt file")
+    void downloadFile() throws IOException {
+        open(TXT_URL);
+        File download = $(TXT_SELECTOR).download();
+        String fileContent = IOUtils.toString(new FileReader(download));
+        assertTrue(fileContent.contains(TXT_TEXT));
+    }
+
+    @Test
+    @DisplayName("Test for PDF file")
+    @Owner("tsareva")
+    @Story("Download, parse and check PDF file")
+    void downloadPdfTest() throws IOException {
+        open(PDF_URL);
+        File file = $(PDF_SELECTOR).download();
+        PDF parsedPdf = new PDF(file);
+        assertTrue(parsedPdf.text.contains(PDF_TEXT));
+        assertEquals(6, parsedPdf.numberOfPages);
+    }
+
+
+    @Test
+    @DisplayName("Test for Excel file")
+    @Owner("tsareva")
+    @Story("Download, parse and check xls file")
+    public void downloadXlsTest() throws FileNotFoundException {
+        open(XLS_URL);
+        File file = $(XLS_SELECTOR).download();
+        XLS parsedXls = new XLS(file);
+
+        boolean checkPassed = parsedXls.excel
+                .getSheetAt(0)
+                .getRow(128)
+                .getCell(0)
+                .getStringCellValue()
+                .contains(XLS_TEXT);
+        assertTrue(checkPassed);
+    }
+
+    @Test
+    @DisplayName("Test for ZIP file")
+    @Owner("tsareva")
+    @Story("Download, parse and check ZIP file")
+    void downloadZipTest() throws IOException {
+        open(ZIP_URL);
+        File file = $(ZIP_SELECTOR).download();
+        try (InputStream is = new FileInputStream(file.getPath());
+             ZipInputStream zip = new ZipInputStream(is)) {
+            ZipEntry entry;
+            while ((entry = zip.getNextEntry()) != null) {
+                assertEquals(ZIP_TEXT, entry.getName());
+            }
+        }
+    }
+}
